44a0f3f061475addeec4f84e68d0d30519e5700e
dichuvichkin <dichuvichkin@skbkontur.ru>
2017-10-17 16:39:16 +0500

    test

--- index.test.js
+++ index.test.js
@@ -4,36 +4,34 @@ const postcss = require('postcss');
 
 const plugin = require('./');
 
-function run(input) {
+function run(input, output) {
     return postcss([plugin()])
         .process(input)
-        .then(result => result.css);
+        .then(result => {
+            expect(result.css).to.deep.equal(output);
+            expect(result.warnings().length).to.deep.equal(0);
+        });
 }
 
+const inAtRule = '@media screen and (min-width: 480px) {\nbody {\nbackground-color: lightgreen;\n}\n}\n';
+const outAtRule = '@media screen and (min-width: 480px) {\r\nbody {\r\nbackground-color: lightgreen;\r\n}\r\n}\r\n';
+
+const inIds = '#main {\nborder: 1px solid black;\n}\n';
+const outIds = '#main {\r\nborder: 1px solid black;\r\n}\r\n';
+
+const inClass = 'ul li {\npadding: 5px;\n}\n';
+const outClass = 'ul li {\r\npadding: 5px;\r\n}\r\n';
+
 describe('Check EOL', () => {
-    const inAtRule = '@media screen and (min-width: 480px) {\nbody {\nbackground-color: lightgreen;\n}\n}\n';
-    const outAtRule = '@media screen and (min-width: 480px) {\r\nbody {\r\nbackground-color: lightgreen;\r\n}\r\n}\r\n';
-
-    const inIds = '#main {\nborder: 1px solid black;\n}\n';
-    const outIds = '#main {\r\nborder: 1px solid black;\r\n}\r\n';
-
-    const inClass = 'ul li {\npadding: 5px;\n}\n';
-    const outClass = 'ul li {\r\npadding: 5px;\r\n}\r\n';
     it('Check atRule EOL', () => {
-        return run(inAtRule).then(scss => {
-            expect(scss).to.deep.equal(outAtRule);
-        });
+        return run(inAtRule, outIds);
     });
 
     it('Check ids EOL', () => {
-        return run(inIds).then(scss => {
-            expect(scss).to.deep.equal(outIds);
-        });
+        return run(inIds, outIds);
     });
 
     it('Check class EOL', () => {
-        return run(inClass).then(scss => {
-            expect(scss).to.deep.equal(outClass);
-        });
+        return run(inClass, outClass);
     });
 });
