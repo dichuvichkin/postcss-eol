95b6b965e07559a89fff249b2692ac5449d30878
dichuvichkin <dichuvichkin@skbkontur.ru>
2017-10-17 14:57:45 +0500

    Bug fix if typeof raws[current] === 'object'

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
 const postcss = require('postcss');
 const { EOL } = require('os');
 
+const getCurrentValue = (raws, current) => {
+    if (typeof raws[current] === 'boolean') {
+        return {
+            [current]: raws[current]
+        };
+    }
+
+    if (typeof raws[current] === 'object') {
+        return {
+            [current]: raws[current].raw.replace(/\r?\n/g, EOL)
+        };
+    }
+
+    return {
+        [current]: raws[current].replace(/\r?\n/g, EOL)
+    };
+};
+
 const replaceToEOL = (raws = {}) =>
-    Object.keys(raws).reduce((previous, current) => {
-        const currentValue =
-            typeof raws[current] === 'boolean' ?
-            {
-                [current]: raws[current]
-            } :
-            {
-                [current]: raws[current].replace(/\r?\n/g, EOL)
-            };
-        return Object.assign(previous, currentValue);
-    }, {});
+    Object.keys(raws).reduce((previous, current) => Object.assign(previous, getCurrentValue(raws, current)), {});
 
 module.exports = postcss.plugin('postcss-eol', () => css => {
     css.raws = replaceToEOL(css.raws);
