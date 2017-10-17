b290f6fee6e61fb4ec45eecbfe9d4221b6fd9d6c
dichuvichkin <dichuvichkin@skbkontur.ru>
2017-10-17 16:13:42 +0500

    test

--- index.test.js
+++ index.test.js
@@ -8,16 +8,15 @@ function run(input) {
         .then(result => result.css);
 }
 
-const inAtRule = '@media screen and (min-width: 480px) {\nbody {\nbackground-color: lightgreen;\n}\n}\n';
-const outAtRule = '@media screen and (min-width: 480px) {\r\nbody {\r\nbackground-color: lightgreen;\r\n}\r\n}\r\n';
-
-const inIds = '#main {\nborder: 1px solid black;\n}\n';
-const outIds = '#main {\r\nborder: 1px solid black;\r\n}\r\n';
-
-const inClass = 'ul li {\npadding: 5px;\n}\n';
-const outClass = 'ul li {\r\npadding: 5px;\r\n}\r\n';
-
 describe('Check EOL', () => {
+    const inAtRule = '@media screen and (min-width: 480px) {\nbody {\nbackground-color: lightgreen;\n}\n}\n';
+    const outAtRule = '@media screen and (min-width: 480px) {\r\nbody {\r\nbackground-color: lightgreen;\r\n}\r\n}\r\n';
+
+    const inIds = '#main {\nborder: 1px solid black;\n}\n';
+    const outIds = '#main {\r\nborder: 1px solid black;\r\n}\r\n';
+
+    const inClass = 'ul li {\npadding: 5px;\n}\n';
+    const outClass = 'ul li {\r\npadding: 5px;\r\n}\r\n';
     it('Check atRule EOL', () => {
         return run(inAtRule).then(scss => {
             expect(scss).toEqual(outAtRule);
