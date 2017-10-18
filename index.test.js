4f1076cda1522853d8e6c496a89fa7d9eeeed179
dichuvichkin <dichuvichkin@skbkontur.ru>
2017-10-18 16:19:58 +0500

    added tests

--- index.test.js
+++ index.test.js
@@ -13,32 +13,94 @@ function run(input, output) {
         });
 }
 
-const inAtRule = '@media screen and (min-width: 480px) {\nbody {\nbackground-color: lightgreen;\n}\n}\n';
-const outAtRule = `@media screen and (min-width: 480px) {${EOL}body {${EOL}background-color: lightgreen;${EOL}}${EOL}}${EOL}`;
+function runLF(input, output) {
+    return postcss([plugin('\n')])
+        .process(input)
+        .then(result => {
+            expect(result.css).toEqual(output);
+            expect(result.warnings().length).toBe(0);
+        });
+}
 
-const inIds = '#main {\nborder: 1px solid black;\n}\n';
-const outIds = `#main {${EOL}border: 1px solid black;${EOL}}${EOL}`;
+function runCRLF(input, output) {
+    return postcss([plugin('\r\n')])
+        .process(input)
+        .then(result => {
+            expect(result.css).toEqual(output);
+            expect(result.warnings().length).toBe(0);
+        });
+}
 
-const inClass = 'ul li {\npadding: 5px;\n}\n';
-const outClass = `ul li {${EOL}padding: 5px;${EOL}}${EOL}`;
+const inAtRule = '@media screen and (min-width: 480px) {\nbody {\nbackground-color: lightgreen;\n}\n}';
+const outAtRuleEOL = `@media screen and (min-width: 480px) {${EOL}body {${EOL}background-color: lightgreen;${EOL}}${EOL}}${EOL}`;
+const outAtRuleLF = '@media screen and (min-width: 480px) {\nbody {\nbackground-color: lightgreen;\n}\n}\n';
+const outAtRuleCRLF = '@media screen and (min-width: 480px) {\r\nbody {\r\nbackground-color: lightgreen;\r\n}\r\n}\r\n';
 
-const inOne = 'ul li {\n\npadding: 5px;\n\n}\n\n';
-const outOne = `ul li {${EOL}${EOL}padding: 5px;${EOL}${EOL}}${EOL}${EOL}`;
+const inIds = '#main {\nborder: 1px solid black;\n}';
+const outIdsEOL = `#main {${EOL}border: 1px solid black;${EOL}}${EOL}`;
+const outIdsLF = '#main {\nborder: 1px solid black;\n}\n';
+const outIdsCRLF = '#main {\r\nborder: 1px solid black;\r\n}\r\n';
+
+const inClass = 'ul li {\npadding: 5px;\n}';
+const outClassEOL = `ul li {${EOL}padding: 5px;${EOL}}${EOL}`;
+const outClassLF = 'ul li {\npadding: 5px;\n}\n';
+const outClassCRLF = 'ul li {\r\npadding: 5px;\r\n}\r\n';
+
+const inOne = 'ul li {\n\npadding: 5px;\n\n}';
+const outOneEOL = `ul li {${EOL}${EOL}padding: 5px;${EOL}${EOL}}${EOL}`;
+const outOneLF = 'ul li {\n\npadding: 5px;\n\n}\n';
+const outOneCRLF = 'ul li {\r\n\r\npadding: 5px;\r\n\r\n}\r\n';
 
 describe('Check EOL', () => {
     it('Check atRule EOL', () => {
-        return run(inAtRule, outAtRule);
+        return run(inAtRule, outAtRuleEOL);
     });
 
     it('Check ids EOL', () => {
-        return run(inIds, outIds);
+        return run(inIds, outIdsEOL);
     });
 
     it('Check class EOL', () => {
-        return run(inClass, outClass);
+        return run(inClass, outClassEOL);
     });
 
     it('Check one more', () => {
-        return run(inOne, outOne);
+        return run(inOne, outOneEOL);
+    });
+});
+
+describe('Check LF', () => {
+    it('Check atRule LF', () => {
+        return runLF(inAtRule, outAtRuleLF);
+    });
+
+    it('Check ids LF', () => {
+        return runLF(inIds, outIdsLF);
+    });
+
+    it('Check class LF', () => {
+        return runLF(inClass, outClassLF);
+    });
+
+    it('Check one more LF', () => {
+        return runLF(inOne, outOneLF);
+    });
+});
+
+describe('Check CRLF', () => {
+    it('Check atRule CRLF', () => {
+        return runCRLF(inAtRule, outAtRuleCRLF);
+    });
+
+    it('Check ids CRLF', () => {
+        return runCRLF(inIds, outIdsCRLF);
+    });
+
+    it('Check class CRLF', () => {
+        return runCRLF(inClass, outClassCRLF);
+    });
+
+    it('Check one more CRLF', () => {
+        return runCRLF(inOne, outOneCRLF);
     });
 });
