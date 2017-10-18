d0c8cbfa2e0e598c673ff6593e8617f554a18c0e
dichuvichkin <dichuvichkin@skbkontur.ru>
2017-10-18 16:19:52 +0500

    added custom EOL and append

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const postcss = require('postcss');
 const { EOL } = require('os');
 
-const getCurrentValue = (raws, current) => {
+const getCurrentValue = (raws, current, eol) => {
     const re = /\r?\n/g;
     if (typeof raws[current] === 'boolean') {
         return {
@@ -11,21 +11,23 @@ const getCurrentValue = (raws, current) => {
 
     if (typeof raws[current] === 'object') {
         return {
-            [current]: raws[current].raw.replace(re, EOL)
+            [current]: raws[current].raw.replace(re, eol)
         };
     }
 
     return {
-        [current]: raws[current].replace(re, EOL)
+        [current]: raws[current].replace(re, eol)
     };
 };
 
-const replaceToEOL = (raws = {}) =>
-    Object.keys(raws).reduce((previous, current) => Object.assign(previous, getCurrentValue(raws, current)), {});
+const replaceToEOL = (raws = {}, eol) => Object.keys(raws).reduce((previous, current) => Object.assign(previous, getCurrentValue(raws, current, eol)), {});
 
-module.exports = postcss.plugin('postcss-eol', () => css => {
-    css.raws = replaceToEOL(css.raws);
+module.exports = postcss.plugin('postcss-eol', (eol = EOL, append = true) => css => {
+    css.raws = replaceToEOL(css.raws, eol);
+    if (append && !new RegExp(eol + '$').test(css.raws.after)) {
+        css.raws.after = eol;
+    }
     css.walk(el => {
-        el.raws = replaceToEOL(el.raws);
+        el.raws = replaceToEOL(el.raws, eol);
     });
 });
